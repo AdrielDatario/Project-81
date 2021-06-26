57caa3d0ae381a1322bc1609b9ef354476d6297f
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-06-25 17:39:25 -0700

    hi

---
+++ main.js
@@ -0,0 +1,32 @@
+var canvas = document.getElementById("my_canvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(250, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "orange";
+ctx.lineWidth = 2;
+ctx.arc(300, 260, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(250, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(250, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 2;
+ctx.arc(250, 210, 40 ,0 , 2*Math.PI);
+ctx.stroke();
